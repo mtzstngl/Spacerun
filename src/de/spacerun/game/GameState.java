86b2d108a8de525377ac6ed5fd2b7e23d604ac07
Matze Stangl <matze749@gmail.com>
2013-05-22 20:42:00 +0200

    add recurring obstacles and a highscore

--- src/de/spacerun/game/GameState.java
+++ src/de/spacerun/game/GameState.java
@@ -22,6 +22,8 @@
 package de.spacerun.game;
 
 import java.awt.Font;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
@@ -37,23 +39,39 @@ import de.spacerun.mainmenu.SimpleFont;
 
 public class GameState extends BasicGameState {
 	private int stateID;
-	private int width, height, score;
+	private int width, height;
 	private SimpleFont font;
 	private Rectangle playerRec;
-	private float playerPos;
+	private float playerPos, playerSpeed;
+	
+	private long startTime;
+	private long score;
+	
+	private int sectorWidth;
+	private Deque<ObstacleRow> obstacles;
+	private float obstacleSpeed, obstacleGap;
 	
 	public GameState(int ID){
 		this.stateID = ID;
 	}
-
+//TODO: provide a way to input your name
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		font = new SimpleFont("Arial", Font.PLAIN, 20);
 		score = 0;
+		playerSpeed = 0.55f;
 		width = gc.getWidth();
 		height = gc.getHeight();
-		playerRec = new Rectangle(width/2 - 10, height - 50, 20, 50);
+		playerRec = new Rectangle(width/2 - 10, height - 50, 20, 50);//TODO: make this relative
 		playerPos = width/2 - 10;
+		
+		startTime = System.currentTimeMillis(); //TODO: Maybe change score system
+		
+		obstacleSpeed = 0.01f;
+		obstacleGap = height / 4;
+		sectorWidth = width / 10;//TODO: make this relative
+		obstacles = new ArrayDeque<ObstacleRow>();
+		obstacles.add(new ObstacleRow(sectorWidth, 10, -40));//TODO: make this relative
 	}
 
 	@Override
@@ -61,14 +79,22 @@ public class GameState extends BasicGameState {
 		//Highscore rendering
 		int tmpWidth, yOffset;
 		
-		tmpWidth = font.get().getWidth(Integer.toString(score));
-		yOffset = font.get().getYOffset(Integer.toString(score));
-		font.get().drawString(width - tmpWidth - yOffset, 0, Integer.toString(score));
+		score = (System.currentTimeMillis() - startTime) / 100;
+		
+		tmpWidth = font.get().getWidth(Long.toString(score));
+		yOffset = font.get().getYOffset(Long.toString(score));
+		font.get().drawString(width - tmpWidth - yOffset, 0, Long.toString(score));
 		
 		//Game rendering
 		g.setColor(Color.cyan);
 		g.fillRect(playerRec.getX(), playerRec.getY(), playerRec.getWidth(), playerRec.getHeight());
 		g.draw(playerRec);
+		
+		if(!obstacles.isEmpty()){
+			for(ObstacleRow i : obstacles){
+				i.draw(g, Color.red);
+			}
+		}
 	}
 
 	@Override
@@ -77,17 +103,32 @@ public class GameState extends BasicGameState {
 		
 		if(input.isKeyDown(Input.KEY_LEFT)){
 			if(playerRec.getX() > 0){
-				playerPos -= 0.4f * delta; //delta is to make sure we travel the same way each render/time
+				playerPos -= playerSpeed * delta; //delta is to make sure we travel the same way each render/frame
 				playerRec.setX(playerPos);
 			}
 		}else if(input.isKeyDown(Input.KEY_RIGHT)){
 			if((playerRec.getX() + playerRec.getWidth()) < width){
-				playerPos += 0.4f * delta;
+				playerPos += playerSpeed * delta;
 				playerRec.setX(playerPos);
 			}
 		}else if(input.isKeyDown(Input.KEY_ESCAPE)){
 			sbg.getState(Spacerun.MAINMENUSTATE).init(gc, sbg);
 			sbg.enterState(Spacerun.MAINMENUSTATE);
+		}
+		
+		if(!obstacles.isEmpty()){
+			if((obstacles.getLast().getPosition() - obstacleGap) >= 0){
+				obstacles.add(new ObstacleRow(sectorWidth, 10, -40));
+			}
+			for(ObstacleRow i : obstacles){
+				i.setPosition(obstacleSpeed * delta);
+				if(i.intersects(playerRec)){
+					System.out.println("you got hit");
+				}
+				if(i.getPosition() >= height){
+					obstacles.remove(i);
+				}
+			}
 		}
 	}
 
