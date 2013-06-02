082ce8eae4df7da1482e2e07df52e1445e4e49d7
Matze Stangl <matze749@gmail.com>
2013-06-02 18:14:03 +0200

    add highscorestate to available selections and fix indent

--- src/de/spacerun/mainmenu/MainMenuState.java
+++ src/de/spacerun/mainmenu/MainMenuState.java
@@ -34,77 +34,80 @@ import org.newdawn.slick.state.StateBasedGame;
 import de.spacerun.main.Spacerun;
 
 public class MainMenuState extends BasicGameState {
-	private SimpleFont font;
-    private int width, index, len;
-    private String[] text;
-    private int stateID;
-    
-    public MainMenuState(int ID) {
-    	this.stateID = ID;
-    }
+  private SimpleFont font;
+  private int width, index, len;
+  private String[] text;
+  private int stateID;
+  
+  public MainMenuState(int ID) {
+  	this.stateID = ID;
+  }
 
-    @Override
-    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-    	index = 0;
-    	text = new String[] {"Spiel starten", "Multiplayer", "Highscores", "Steuerung", "Exit"};
-        len = text.length;
-        font = new SimpleFont("Arial", Font.PLAIN, 20);
+  @Override
+  public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+  	index = 0;
+    text = new String[] {"Spiel starten", "Multiplayer", "Highscores", "Steuerung", "Exit"};
+    len = text.length;
+    font = new SimpleFont("Arial", Font.PLAIN, 20);
 
-        width = gc.getWidth();
-    }
-        
-    @Override
-    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) {
-    	float tmpWidth, tmpHeight;
-        float start = 20;
+    width = gc.getWidth();
+  }
+ 
+  @Override
+  public void render(GameContainer gc, StateBasedGame sbg, Graphics g) {
+    float tmpWidth, tmpHeight;
+    float start = 20;
 
-        for(int i = 0; i < len; i++){
-        	tmpWidth = font.get().getWidth(text[i]);
-        	tmpHeight = font.get().getHeight(text[i]);
-        	
-        	if(i == index){
-        		font.get().drawString(width/2 - tmpWidth/2, start, text[i], Color.red);
-        	}else{
-        		font.get().drawString(width/2 - tmpWidth/2, start, text[i]);
-        	}
-        	
-        	start += tmpHeight + 20;
-        }
-    }
-    
-    @Override
-    public void update (GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-    	Input input = gc.getInput();
+    for(int i = 0; i < len; i++){
+  	  tmpWidth = font.get().getWidth(text[i]);
+  	  tmpHeight = font.get().getHeight(text[i]);
     	
-        if(input.isKeyPressed(Input.KEY_UP)){
-        	if(index > 0){
-                index--;
-        	}
-        }else if(input.isKeyPressed(Input.KEY_DOWN)){
-        	if(index < (len -1)){
-                index++;
-        	}
-        }else if(input.isKeyPressed(Input.KEY_ENTER)){
-        	if(text[index] == "Spiel starten"){
-        		sbg.getState(Spacerun.GAMESTATE).init(gc, sbg);
-        		sbg.enterState(Spacerun.GAMESTATE);
-        	}else if(text[index] == "Multiplayer"){
-        		//sbg.getState(Spacerun.MULTIPLAYER).init(gc, sbg);
-        		//sbg.enterState(Spacerun.MULTIPLAYER);
-        	}else if(text[index] == "Highscores"){
-        		//sbg.getState(Spacerun.HIGHSCORESTATE).init(gc, sbg);
-        		//sbg.enterState(Spacerun.HIGHSCORESTATE);
-        	}else if(text[index] == "Steuerung"){
-        		//sbg.getState(Spacerun.CONTROL).init(gc, sbg);
-        		//sbg.enterState(Spacerun.CONTROL);
-        	}else if(text[index] == "Exit"){
-        		gc.exit();
-        	}
-        }
+    	if(i == index){
+    	  font.get().drawString(width/2 - tmpWidth/2, start, text[i], Color.red);
+    	}else{
+    	  font.get().drawString(width/2 - tmpWidth/2, start, text[i]);
+    	}
+    	
+    	start += tmpHeight + 20;
     }
+  }
+  
+  @Override
+  public void update (GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+  	Input input = gc.getInput();
+  	
+    if(input.isKeyPressed(Input.KEY_UP)){
+  	  if(index > 0){
+        index--;
+  	  }
+    }else if(input.isKeyPressed(Input.KEY_DOWN)){
+      if(index < (len -1)){
+        index++;
+      }
+    }else if(input.isKeyPressed(Input.KEY_ENTER)){
+  	  if(text[index] == "Spiel starten"){
+  		  sbg.getState(Spacerun.GAMESTATE).init(gc, sbg);
+  		  sbg.enterState(Spacerun.GAMESTATE);
+    	}else if(text[index] == "Multiplayer"){
+    	  //sbg.getState(Spacerun.MULTIPLAYERSTATE).init(gc, sbg);
+  		  //sbg.enterState(Spacerun.MULTIPLAYERSTATE);
+  	  }else if(text[index] == "Highscores"){
+  	    sbg.getState(Spacerun.HIGHSCORESTATE).init(gc, sbg);
+    	  sbg.enterState(Spacerun.HIGHSCORESTATE);
+      }else if(text[index] == "Steuerung"){
+  		  //sbg.getState(Spacerun.CONTROLSTATE).init(gc, sbg);
+  		  //sbg.enterState(Spacerun.CONTROLSTATE);
+  		}else if(text[index] == "Exit"){
+    	  gc.exit();
+  	  }   
+    }
+    if(input.isKeyPressed(Input.KEY_ESCAPE)){
+  	  gc.exit();
+    }
+  }
 
-	@Override
-	public int getID() {
-		return stateID;
+  @Override
+  public int getID() {
+	  return stateID;
 	}
 }
